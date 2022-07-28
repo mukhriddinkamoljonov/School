63cc913c0157d611d701a0dffcb15e4b9d72cbcc
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-07-28 11:12:58 +0500

    sdf

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import './App.css';
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
-import "./App.css";
-
-// import required modules
-import { Mousewheel, Pagination } from "swiper";
-
-export default function App() {
+function App() {
   return (
-    <>
-      <Swiper
-        direction={"vertical"}
-        slidesPerView={1}
-        spaceBetween={30}
-        mousewheel={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Mousewheel, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
   );
 }
+
+export default App;
