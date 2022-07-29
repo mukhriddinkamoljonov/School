de89ac4cb3e151a617e8ff46333146cc84c2dfd0
Muhriddin Kamoljonov <84762506+mukhriddinkamoljonov@users.noreply.github.com>
2022-07-29 15:40:37 +0500

    chcjehvjv

---
+++ src/Pages/umrah/Umrah.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Umrah = () => {
+  return (
+    <div className="w-full h-screen">
+    <div className="w-full h-screen">
+
+    </div>
+    
+    </div>
+  )
+}
+
+export default Umrah
