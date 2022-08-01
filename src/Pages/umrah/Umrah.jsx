80fb782b0b40e3a62242c069fa1c228815ce9b46
Muhriddin Kamoljonov <84762506+mukhriddinkamoljonov@users.noreply.github.com>
2022-08-01 15:03:58 +0500

    mdsn sjdn

--- src/Pages/umrah/Umrah.jsx
+++ src/Pages/umrah/Umrah.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import umrah from "../../assets/"
 
 const Umrah = () => {
   return (
     <div className="w-full h-screen">
-    <div className="w-full h-screen">
-
+    <div className="w-full h-[643px] bg-zinc-900">
+    <div className="">
+      <h1>Umra ziyorati uchun hozir band qiling!</h1>
     </div>
+    <div className="">
+      <img src={umrah}/>
+    </div>
+    </div>
+  
     
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
+  mode:'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -7,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+}   
